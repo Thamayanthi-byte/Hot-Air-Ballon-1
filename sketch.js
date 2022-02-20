19da7dc23415f735ae5623a0adbaadde6c115e19
Thamayanthi-byte <72857010+Thamayanthi-byte@users.noreply.github.com>
2022-02-20 14:04:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+
+
+function preload(){
+bgImg = loadImage("assets/bg.png")
+
+balloonImg = loadAnimation("assets/balloon1.png","assets/balloon2.png","assets/balloon3.png")
+}
+
+function setup(){
+
+//background image
+
+
+//creating top and bottom grounds
+
+
+
+      
+//creating balloon     
+
+
+
+
+}
+
+function draw() {
+  
+  background("black");
+        
+          //making the hot air balloon jump
+       
+}
